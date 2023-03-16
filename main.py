a9de0fab13e80d7aab41780be7f2e10a900875cc
SL8620 <90258551+SL8620@users.noreply.github.com>
2023-03-16 10:46:24 +0800

    Add files via upload
    第二版代码，将原来的warpAffine更换为warpPerspective，同时支持框选大于电脑分辨率的图片

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ class PyQtMainEntry(QMainWindow, Ui_MainWindow):
         self.boxList = [0, 0, 0, 0]  # 和楼上存一样的东西，后期转numpy对象用到
         self.mouseWindowName = 'BoxPick'  # 选边框的窗口名，不要轻易删
         self.img = None  # 原图像
+        self.resizedImg = None  # 当图像大于电脑屏幕时需要缩放（手机像素太高）
         self.boxedImg = None  # 显示裁剪范围的图像
         self.resultImg = None  # 处理结果
+        self.scale = 1  # 缩放倍数，后期定点还原需要用到
 
     def pickPic_clicked(self):
         # 打开文件选取对话框
@@ -31,8 +33,18 @@ class PyQtMainEntry(QMainWindow, Ui_MainWindow):
             self.boxedImg = self.img.copy()  # 忘了当初为啥要用boxedImg来显示了
             self.boxedImg = cv.cvtColor(self.boxedImg, cv.COLOR_BGR2RGB)  # OpenCV图像以BGR通道存储，显示时需要从BGR转到RGB
             self.picSize = self.boxedImg.shape  # 获取图像大小等信息
+            print(self.picSize)
+
+            self.resizedImg = self.img
+            if self.picSize[0] > 800:
+                self.scale = self.picSize[0] / 800
+                print(self.scale)
+                print([800, int(self.picSize[1] / self.scale)])
+                self.resizedImg = cv.resize(self.img, None, fx=1 / self.scale, fy=1 / self.scale)
+
             rows, cols, channels = self.boxedImg.shape  # 重复了，怎么写的，脑袋坏了吗
             bytesPerLine = channels * cols
+
             QImg = QImage(self.boxedImg.data, cols, rows, bytesPerLine, QImage.Format_RGB888)
             # 在UI界面rawPic显示原始图片
             self.rawPic.setPixmap(
@@ -41,32 +53,32 @@ class PyQtMainEntry(QMainWindow, Ui_MainWindow):
     def on_mouse(self, event, x, y, flags, param):
         # 监听鼠标事件
         if event == cv.EVENT_LBUTTONDOWN:  # 别看这种写法比较冗余，但这是手残党的福音，谁也没办法确定一次就能选好那个点
-            if x < self.picSize[1] / 2 and y < self.picSize[0] / 2:  # 左上
+            if x < self.picSize[1] / self.scale / 2 and y < self.picSize[0] / self.scale / 2:  # 左上
                 self.box[0] = (x, y)
                 self.boxList[0] = [x, y]
-            elif x < self.picSize[1] / 2 and y > self.picSize[0] / 2:  # 左下
+            elif x > self.picSize[1] / self.scale / 2 and y < self.picSize[0] / self.scale/ 2:  # 右上
                 self.box[1] = (x, y)
                 self.boxList[1] = [x, y]
-            elif x > self.picSize[1] / 2 and y < self.picSize[0] / 2:  # 右上
+            elif x < self.picSize[1] / self.scale / 2 and y > self.picSize[0] / self.scale/ 2:  # 左下
                 self.box[2] = (x, y)
                 self.boxList[2] = [x, y]
-            elif x > self.picSize[1] / 2 and y > self.picSize[0] / 2:  # 右下
+            elif x > self.picSize[1] / self.scale / 2 and y > self.picSize[0] / self.scale/ 2:  # 右下
                 self.box[3] = (x, y)
                 self.boxList[3] = [x, y]
 
             # print(self.box)
 
-            img_circle = self.img.copy()
+            img_circle = self.resizedImg.copy()
             # 画圆
             for pos in self.box:
                 if pos != 0:
                     cv.circle(img_circle, pos, 10, (0, 255, 0), thickness=3)
             # 划线
             img_line = img_circle.copy()
-            if (self.box[0] != 0 and self.box[2] != 0):
-                cv.line(img_line, self.box[0], self.box[2], (0, 0, 255), thickness=5)
             if (self.box[0] != 0 and self.box[1] != 0):
                 cv.line(img_line, self.box[0], self.box[1], (0, 0, 255), thickness=5)
+            if (self.box[0] != 0 and self.box[2] != 0):
+                cv.line(img_line, self.box[0], self.box[2], (0, 0, 255), thickness=5)
             if (self.box[2] != 0 and self.box[3] != 0):
                 cv.line(img_line, self.box[2], self.box[3], (0, 0, 255), thickness=5)
             if (self.box[1] != 0 and self.box[3] != 0):
@@ -75,29 +87,38 @@ class PyQtMainEntry(QMainWindow, Ui_MainWindow):
             cv.imshow(self.mouseWindowName, img_line)
             # 下面是在boxedPic一览显示框选后图像的，这样的代码算上刚才的被编译器提示一共有三段重复，编译器都在嘲讽我，能封装还是封装一下吧
             self.boxedImg = cv.cvtColor(img_line, cv.COLOR_BGR2RGB)  # OpenCV图像以BGR通道存储，显示时需要从BGR转到RGB
-            self.picSize = self.boxedImg.shape
-            # print(self.picSize)
             rows, cols, channels = self.boxedImg.shape
             bytesPerLine = channels * cols
             QImg = QImage(self.boxedImg.data, cols, rows, bytesPerLine, QImage.Format_RGB888)
-            self.boxedPic.setPixmap(
-                QPixmap.fromImage(QImg).scaled(self.boxedPic.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
+            self.boxedPic.setPixmap( QPixmap.fromImage(QImg).scaled(self.boxedPic.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
 
     def pickBox_clicked(self):  # 点了选择边框之后会发生的事
         if self.picPath:
             cv.namedWindow(self.mouseWindowName)  # 先创建一个窗口
             cv.setMouseCallback(self.mouseWindowName, self.on_mouse)  # 开始监听鼠标事件
-            cv.imshow(self.mouseWindowName, self.img)  # 在这上面首次显示初始图片，会被后续的覆盖掉
+            cv.imshow(self.mouseWindowName, self.resizedImg)  # 在这上面首次显示初始图片，会被后续的覆盖掉
             cv.waitKey(0)  # 点x才能退出
 
     def resultPic_clicked(self):  # 点了处理图片后会发生的事
+        # print(self.boxList)
+        for point in self.boxList:
+            point[0] = int(point[0] * self.scale)
+            point[1] = int(point[1] * self.scale)
+        # print(self.boxList)
+        testIMG = self.img.copy()
+        for point in self.boxList:
+            cv.circle(testIMG, point, 10, (0, 255, 0), thickness=3)
+            #cv.imshow("scale",testIMG)
+        cv.imwrite('test.png',testIMG)
         imgbak = np.array(self.img)  # 先转化成numpy里的array
-        destPoint = [(0, 0), (0, self.picSize[0]), (self.picSize[1], self.picSize[0])]  # 新图片目标点，大小和原图片是一样的
-        pointList = [self.boxList[0], self.boxList[1], self.boxList[3]]  # 原图片目标点，要与楼上对应，这里选的是左上左下右下三点
+        destPoint = [(0, 0), (self.picSize[1],0), (0, self.picSize[0]), (self.picSize[1], self.picSize[0])]  # 新图片目标点，大小和原图片是一样的
+        pointList = [self.boxList[0], self.boxList[1], self.boxList[2], self.boxList[3]]  # 原图片目标点，要与楼上对应，这里选的是左上左下右下三点
+        # (destPoint)
+        # print(pointList)
         pts1 = np.float32(pointList)  # 听说转成float更好处理？
         pts2 = np.float32(destPoint)
-        M = cv.getAffineTransform(pts1, pts2)  # 本项目最核心的一句话，计算仿射变换矩阵
-        dst = cv.warpAffine(imgbak, M, (self.picSize[1], self.picSize[0]))  # 将仿射变换矩阵应用到原图像，生成目标图像
+        M = cv.getPerspectiveTransform(pts1, pts2)  # 本项目最核心的一句话，计算仿射变换矩阵
+        dst = cv.warpPerspective(imgbak, M, (self.picSize[1], self.picSize[0]))  # 将仿射变换矩阵应用到原图像，生成目标图像
         self.resultImg = dst.copy()  # 保存一下
 
         Img = cv.cvtColor(dst, cv.COLOR_BGR2RGB)  # OpenCV图像以BGR通道存储，显示时需要从BGR转到RGB
